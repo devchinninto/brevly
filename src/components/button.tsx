122b67934af9b41fe14bc81c2c1fc78b09e17531
marcelle alves <marcellealves.dev@gmail.com>
2026-09-05 18:45:10 -0300

    feat: improve Button component with Tailwind variants

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,13 +1,30 @@
 import type { ComponentProps } from 'react'
+import { tv, type VariantProps } from 'tailwind-variants'
 
-type ButtonProps = ComponentProps<'button'>
+const button = tv({
+  base: 'flex h-12 items-center justify-center rounded-lg px-4 text-sm  transition-colors disabled:pointer-events-none disabled:opacity-50',
+  variants: {
+    variant: {
+      primary: 'bg-blue-base font-bold  text-white hover:bg-blue-dark',
+      secondary:
+        'bg-gray-200 text-gray-500 hover:bg-gray-300 w-[fit-content] h-[32px]'
+    }
+  },
+  defaultVariants: {
+    variant: 'primary'
+  }
+})
 
-export function Button({ children, className, ...props }: ButtonProps) {
+type ButtonProps = ComponentProps<'button'> & VariantProps<typeof button>
+
+export function Button({
+  children,
+  className,
+  variant,
+  ...props
+}: ButtonProps) {
   return (
-    <button
-      className="flex h-12 items-center justify-center rounded-lg bg-blue-base px-4 text-sm font-bold text-white transition-colors hover:bg-blue-dark disabled:pointer-events-none disabled:opacity-50"
-      {...props}
-    >
+    <button className={button({ variant, className })} {...props}>
       {children}
     </button>
   )

1a7a445203813b8601e691f7832c7eec9e08c2c4
marcelle alves <marcellealves.dev@gmail.com>
2026-09-05 19:01:32 -0300

    feat: add LinkIcon and message for empty state in LinkList component

--- src/components/link-list.tsx
+++ src/components/link-list.tsx
@@ -1,9 +1,9 @@
 import { Button } from './button'
-import { DownloadSimpleIcon } from '@phosphor-icons/react'
+import { DownloadSimpleIcon, LinkIcon } from '@phosphor-icons/react'
 
 export function LinkList() {
   return (
-    <section className="flex w-full flex-col gap-6 rounded-lg bg-gray-100 p-6 md:p-8 lg:w-145">
+    <section className="flex w-full flex-col gap-6 rounded-lg bg-gray-100 p-6 md:p-8 lg:w-145 ">
       <div className="flex place-content-between">
         <h1 className="text-lg font-bold text-gray-600">Meus links</h1>
         <Button variant="secondary">
@@ -12,6 +12,12 @@ export function LinkList() {
             Baixar CSV
           </div>
         </Button>
+      </div>
+      <div className="flex flex-col gap-4 justify-center items-center border-t border-gray-300 pt-5">
+        <LinkIcon strokeWidth={1.5} className="size-8 text-gray-400" />
+        <span className="text-gray-500 text-xs uppercase">
+          AINDA NÃO EXISTEM LINKS CADASTRADOS
+        </span>
       </div>
     </section>
   )

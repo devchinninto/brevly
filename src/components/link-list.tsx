f77d9905e201249703edb64e826524eba08570f2
marcelle alves <marcellealves.dev@gmail.com>
2026-09-05 18:29:09 -0300

    feat: add LinkList component to display user links

---
+++ src/components/link-list.tsx
@@ -0,0 +1,7 @@
+export function LinkList() {
+  return (
+    <section className="flex w-full flex-col gap-6 rounded-lg bg-gray-100 p-6 md:p-8 lg:w-145">
+      <h1 className="text-lg font-bold text-gray-600">Meus links</h1>
+    </section>
+  )
+}

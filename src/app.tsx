ada76207dd828a0d1707dfebb9e7194f4ebb0a44
marcelle alves <marcellealves.dev@gmail.com>
2026-09-05 18:28:45 -0300

    feat: restructure App component layout and integrate LinkList

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,21 @@
 import { AddLink } from './components/add-link'
+import { LinkList } from './components/link-list'
+import logo from './assets/logo.svg'
 
 export function App() {
   return (
-    <main className="h-dvh flex flex-col items-center justify-center p-10 bg-gray-200 ">
-      <AddLink />
-    </main>
+    <div className="flex min-h-dvh justify-center bg-gray-200 px-3 py-8 md:px-6 lg:items-center">
+      <main className="flex w-full max-w-145 flex-col gap-6 lg:max-w-fit">
+        <img
+          src={logo}
+          alt="brev.ly"
+          className="h-6 w-auto self-center lg:self-start"
+        />
+        <div className="flex flex-col items-center gap-3 lg:flex-row lg:items-start lg:gap-5">
+          <AddLink />
+          <LinkList />
+        </div>
+      </main>
+    </div>
   )
 }

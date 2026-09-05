ea3e8d58d4953beb7b0050b2dfee260db9fe895d
marcelle alves <marcellealves.dev@gmail.com>
2026-09-05 19:31:58 -0300

    feat: implement empty state handling and copy functionality in LinkList component

--- src/components/ui/link-list.tsx
+++ src/components/ui/link-list.tsx
@@ -1,7 +1,27 @@
 import { Button } from './button'
 import { DownloadSimpleIcon, LinkIcon } from '@phosphor-icons/react'
+import { LinkCard, type Link } from './link-card'
+
+const links: Link[] = [
+  {
+    id: '1',
+    shortUrl: 'brev.ly/portfolio',
+    originalUrl: 'example.portfolio.com.br/example-user-123456',
+    accessCount: 15
+  }
+]
 
 export function LinkList() {
+  const isEmpty = links.length === 0
+  const isEmptyMessage = 'AINDA NÃO EXISTEM LINKS CADASTRADOS'
+
+  function handleCopy(link: Link) {
+    navigator.clipboard.writeText(`https://${link.shortUrl}`)
+  }
+
+  // function handleDelete(link: Link) {
+  // }
+
   return (
     <section className="flex w-full flex-col gap-6 rounded-lg bg-gray-100 p-6 md:p-8 lg:w-145 ">
       <div className="flex place-content-between">
@@ -13,12 +33,27 @@ export function LinkList() {
           </div>
         </Button>
       </div>
-      <div className="flex flex-col gap-4 justify-center items-center border-t border-gray-300 pt-5">
-        <LinkIcon strokeWidth={1.5} className="size-8 text-gray-400" />
-        <span className="text-gray-500 text-xs uppercase">
-          AINDA NÃO EXISTEM LINKS CADASTRADOS
-        </span>
-      </div>
+
+      {isEmpty ? (
+        <div className="flex flex-col gap-4 justify-center items-center border-t border-gray-300 pt-5">
+          <LinkIcon strokeWidth={1.5} className="size-8 text-gray-400" />
+          <span className="text-gray-500 text-xs uppercase">
+            {isEmptyMessage}
+          </span>
+        </div>
+      ) : (
+        <ul className="flex flex-col divide-y divide-gray-300 border-t border-gray-300">
+          {links.map((link) => (
+            <li key={link.id}>
+              <LinkCard
+                link={link}
+                onCopy={handleCopy}
+                // onDelete={handleDelete}
+              />
+            </li>
+          ))}
+        </ul>
+      )}
     </section>
   )
 }

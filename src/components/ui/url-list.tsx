da97494b232149e48e1fdc511a5021f2b19d9257
marcelle alves <marcellealves.dev@gmail.com>
2026-09-24 19:57:04 -0300

    feat: add CSV download functionality for URL reports

--- src/components/ui/url-list.tsx
+++ src/components/ui/url-list.tsx
@@ -2,6 +2,7 @@ import { Button } from './button'
 import { DownloadSimpleIcon, LinkIcon } from '@phosphor-icons/react'
 import { UrlCard } from './url-card'
 import { useUrlStore } from '../../store/url-store'
+import { downloadUrlsReport } from '../../http/export-csv'
 
 export function UrlList() {
   const urls = useUrlStore((state) => state.urls)
@@ -14,7 +15,11 @@ export function UrlList() {
     <section className="flex w-full flex-col gap-6 rounded-lg bg-gray-100 p-6 md:p-8 lg:w-145 ">
       <div className="flex place-content-between">
         <h1 className="text-lg font-bold text-gray-600">Meus links</h1>
-        <Button variant="secondary" disabled={isEmpty}>
+        <Button
+          variant="secondary"
+          disabled={isEmpty}
+          onClick={downloadUrlsReport}
+        >
           <div className="flex gap-1 justify-center items-center">
             <DownloadSimpleIcon />
             Baixar CSV
